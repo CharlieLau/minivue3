a0ee7ab4e5adf0bcd83d2779aaedabce53785bf6
liuqingqing05 <liuqingqing05@meituan.com>
2020-07-16 20:38:24 +0800

    feat: 基建

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ],
+    },
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    mode: 'development',
+    devServer: {
+        port: 9003
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Development',
+            template:'public/index.html'
+        })
+    ]
+}
